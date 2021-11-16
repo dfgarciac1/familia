d09be709f8310ed95ef4c91e07b33ea274eb6657
DAVID GARCIA <dfgarciac1@eafit.edu.co>
2021-11-16 15:58:54 -0500

    Remove

--- utils/carrousel_data.tsx
+++
@@ -1,7 +0,0 @@
-import  a from '../images/a.jpg'
-import b from '../images/Group 1.svg'
-
-export const images =
-[
-    {title: 'San Diego', subtitle:'This is San Diego' , img:a},
-    {title: 'San Diego', subtitle:'This is San Diego' , img:b}]
